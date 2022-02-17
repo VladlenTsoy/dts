4f7792f83446abae0bd28343f70299c53ab2dc94
vladlen <vladlengg@gmail.com>
2022-02-18 01:38:24 +0500

    add image, icon and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum dolor doloribus iusto rerum,
+                    veniam voluptas. Ab cum fugiat impedit ipsa mollitia odit quod? At cumque minima nesciunt odio vel.
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum eius facere in, iste magnam nisi
+                    pariatur rerum similique! Accusamus ad error magnam quibusdam voluptate? Consequatur ipsa minima
+                    praesentium quia sequi?
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
