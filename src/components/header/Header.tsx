2f9941b1538432541459c4ef90547ac16490a15b
vladlen <vladlengg@gmail.com>
2022-02-24 01:09:37 +0500

    update media header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,26 +2,33 @@ import React from "react"
 import styles from "./Header.module.css"
 import {Link} from "react-scroll"
 import ButtonNavigation from "./ButtonNavigation"
+import {useMediaQuery} from "react-responsive"
 
 const Header = () => {
+    const isDesktopOrMobile = useMediaQuery({
+        query: "(min-width: 767px)"
+    })
+
     return (
         <div className={styles.header}>
             <img src="/images/logo.svg" alt="DTS" width="247" height="82" />
-            <ButtonNavigation />
-            <ol className={styles.navigation}>
-                <li>
-                    <Link to="about-us" smooth>About Us</Link>
-                </li>
-                <li>
-                    <Link to="out-mission" smooth>Our Mission</Link>
-                </li>
-                <li>
-                    <Link to="services" smooth>Services</Link>
-                </li>
-                <li>
-                    <Link to="contact" smooth>Contact</Link>
-                </li>
-            </ol>
+            {!isDesktopOrMobile ?
+                <ButtonNavigation /> :
+                <ol className={styles.navigation}>
+                    <li>
+                        <Link to="about-us" smooth>About Us</Link>
+                    </li>
+                    <li>
+                        <Link to="out-mission" smooth>Our Mission</Link>
+                    </li>
+                    <li>
+                        <Link to="services" smooth>Services</Link>
+                    </li>
+                    <li>
+                        <Link to="contact" smooth>Contact</Link>
+                    </li>
+                </ol>
+            }
         </div>
     )
 }
