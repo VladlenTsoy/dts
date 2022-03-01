748d0455da0f0b1bf9c6d4e391f6d50255a7a0b4
vladlen <vladlengg@gmail.com>
2022-03-02 03:17:34 +0500

    add google map

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,11 +1,33 @@
 import React from "react"
 import styles from "./Contact.module.css"
 import {Element} from "react-scroll"
+import {GoogleMap, LoadScript, Marker} from "@react-google-maps/api"
+
+const containerStyle = {
+    width: "100%",
+    height: "100%"
+}
+
+const center = {
+    lat: 38.63909982968709,
+    lng: -121.37462766057325
+}
 
 const Contact = () => {
     return (
         <Element name="contact" className={styles.contact}>
             <div className={styles.map}>
+                <LoadScript
+                    googleMapsApiKey="AIzaSyBSNRntfWg3WQpwR8JWs_36lIXUFbIKpcI"
+                >
+                    <GoogleMap
+                        mapContainerStyle={containerStyle}
+                        center={center}
+                        zoom={17}
+                    >
+                        <Marker position={center} />
+                    </GoogleMap>
+                </LoadScript>
             </div>
             <div className={styles.content}>
                 <h2>Contact</h2>
